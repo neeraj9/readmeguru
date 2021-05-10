8140c35012d2a161efa850843e16421be5537e58
Neeraj Sharma <neerajsharma.9@outlook.com>
2021-05-10 23:21:06 +0530

    go to top of page via bottom right icon

--- js/script.js
+++ js/script.js
@@ -38,4 +38,27 @@ $(function () {
 
   window.requestAnimationFrame(moveHeader)
 
+  // got to top of the page button
+  //Get the button:
+  topButton = document.getElementById("topBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {
+    scrollFunction()
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topButton.style.display = "block";
+    } else {
+      topButton.style.display = "none";
+    }
+  }
+
 }) // when page loads
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
